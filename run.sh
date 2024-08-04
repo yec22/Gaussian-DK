d11c56ba09090707def3b8e77aa582eeefcd2675
Sheng Ye <yec22@mails.tsinghua.edu.cn>
2024-08-04 18:20:50 +0800

    v1.0

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-CUDA_VISIBLE_DEVICES=0 python train.py -r 4 -s datasets/dark/piano -m output/ablation/wo-grad-scale/piano --port 1111 --eval  # Train with train/test split
-CUDA_VISIBLE_DEVICES=0 python render.py -m output/handcraft/piano # Generate renderings
-CUDA_VISIBLE_DEVICES=0 python metrics.py -m output/ablation/wo-grad-scale/piano # Compute error metrics on renderings
-CUDA_VISIBLE_DEVICES=0 python render_spherify.py -m output/handcraft/piano
+# Train with train/test split
+python train.py -r 4 -s datasets/dark/piano -m output/piano --port 1111 --eval
+
+# Generate renderings
+python render.py -m output/piano
+
+# Compute metrics on renderings
+python metrics.py -m output/piano
+
+# More visualization
+python render_spherify.py -m output/piano
